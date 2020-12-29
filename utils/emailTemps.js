bddfcfb025c39b569930dfe64cbb56fbaca1451b
Pezhman Ghavami <pejmanghavami@gmail.com>
2020-12-29 22:00:16 +0100

    Added the email templates

--- utils/emailTemps.js
+++ utils/emailTemps.js
@@ -59,7 +59,78 @@ const newUser = {
                 <h3>Welcome to YelpCamp!</h3>
                 <a href="${verifyAccURL}">Please verify Your Account by clicking on this link.</a>
                 <p>If the link above doesn't work please use copy and paste this link into your browser and press enter.</p>
-                ${verifyAccURL}
+                <p>${verifyAccURL}</p>
+                <a href="http://127.0.0.1:300/campgrounds">Start browsing our campgrounds now!</a>
+            </main>
+        </body>
+        </html>`
+    }
+}
+
+const forgotPassword = {
+    subject: "🏕️ Forgot Password - YelpCamp 🏕️",
+    text: function (verifyAccURL) {
+        return `To reset Your password use the following link.
+        ${verifyAccURL}
+        If you didn't made this request please ignore this email.`
+    },
+    html: function (verifyAccURL) {
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Forgot Password</title>
+            <style>
+                body{
+                    text-align: center;
+                    background-color: #353535;
+                    color: white;
+                }
+                a {
+                    color: #4f82ce;
+                }
+            </style>
+        </head>
+        <body>
+            <main>
+                <h3>Forgot Password</h3>
+                <a href="${verifyAccURL}">Please click on this link to reset your password.</a>
+                <p>If the link above doesn't work please use copy and paste this link into your browser and press enter.</p>
+                <p>${verifyAccURL}</p>
+                <h3>If you didn't made this request please igonre this email.</h3>
+            </main>
+        </body>
+        </html>`
+    }
+}
+
+const passwordChanged = {
+    subject: "🏕️ You have successfully changed your password - YelpCamp 🏕️",
+    text: function (a) {
+        return `Your password has been successfully changed.`
+    },
+    html: function (a) {
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Password Reseted</title>
+            <style>
+                body{
+                    text-align: center;
+                    background-color: #353535;
+                    color: white;
+                }
+                a {
+                    color: #4f82ce;
+                }
+            </style>
+        </head>
+        <body>
+            <main>
+                <h3>Your password has been successfully changed!</h3>
                 <a href="http://127.0.0.1:300/campgrounds">Start browsing our campgrounds now!</a>
             </main>
         </body>
